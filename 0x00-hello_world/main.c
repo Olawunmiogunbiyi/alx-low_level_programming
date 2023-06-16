87e96a7891c37e1e3f913f7c7877ae48d452fb4d
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-06-16 03:38:33 -0700

    1-compiler

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,12 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+return (0);
+}
+julien@ubuntu:~/c/0x00$ ./1-compiler 
+julien@ubuntu:~/c/0x00$ ls
+0-preprocessor  1-compiler   c
