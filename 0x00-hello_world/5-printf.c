cee16d8c2162358b7fa98cab5f74383625ebbbee
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-06-16 08:41:01 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.hr>
+/**
+*main - entry point
+*Return: always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
