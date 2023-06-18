6b2200ef0d8a1afe28dc45f97d994da47b3bfa07
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-06-18 16:24:45 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+* main - Determines if a number is positive, negative or zero.
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int n:
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+(
+printf(d is poitive\n".n):
+)
+else
+{
+printf(" % d is negative\n",n):
+}
+return (0):
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,6 @@
+0x01. C - Variables, if, else, while
+The last digit
+ I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+ alphABET
+ When I was having that alphabet soup, I never thought that it would pay off
+ Numbers
