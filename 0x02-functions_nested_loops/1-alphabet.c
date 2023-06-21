997c3fc6b54ade7442a007000527c1524e512e40
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-06-21 08:47:52 -0700

    README.md

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - prints the alphabet in lowercase
+* Return: Always 0 (success)
+*/ 
+void print.alphabet(void)
+{
+int i;
+for (1 = 97; 1 <= 122; 1++)
+(
+-putchar(1);
+)
+_putchar('\n');
+}
