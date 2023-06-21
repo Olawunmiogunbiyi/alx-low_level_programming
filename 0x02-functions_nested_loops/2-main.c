55faa285837718ae3dd3e93efdde1cb295d40111
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-06-21 13:51:58 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
+
 /**
  * main - check the code.
  *
- * return: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
     print_alphabet_x10();
-    retutn(0);
+    return (0);
 }
