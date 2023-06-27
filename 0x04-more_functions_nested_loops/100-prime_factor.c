94fb81c707686eac4937decfc246e81e3ef9cec2
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-06-27 09:58:11 -0700

    100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,47 +1,24 @@
-#include "main.h"
-/**
- * print_times_table - prints the times table for n.
- * @n: The multiplication table requested.
- * Return: Nothing.
- */
-void print_times_table(int n)
-{
-	int i, j, res;
+#include <stdio.h>
 
-	if (!(n > 15 || n < 0))
+/**
+ * main -  calculate largest prime of 612852475143
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	long int num, prime;
+
+	num = 612852475143;
+	for (prime = 2; prime <= num; prime++)
 	{
-		for (i = 0; i <= n; i++)
+		if (num % prime == 0)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				res = (i * j);
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (res < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100 && j != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
-				}
-				else
-					_putchar((res % 10) + '0');
-			}
-			_putchar('\n');
+			num /= prime;
+			prime--;
 		}
 	}
+	printf("%ld\n", prime);
+	return (0);
 }
