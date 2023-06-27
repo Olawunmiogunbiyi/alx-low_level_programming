344c7f78ce7bc0154060b3babbf56b0051010bdb
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-06-27 10:03:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
-*
-*Return: Always1(Success)
-*/
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+
 int main(void)
 {
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
+	return (1);
 }
