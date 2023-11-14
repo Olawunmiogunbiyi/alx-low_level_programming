5f86a2be7c8da90aea97ede693f6900711ae1b51
olawunmiogunbiyi <olawunmiogunbiyi153@gmail.com>
2023-11-14 05:12:59 -0800

    hacking injection

---
+++ 0x18-dynamic_libraries/hack.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("9 8 10 24 75 +9\n");
+    printf("congratulation, you win the jackpot!\n");
+    exit(EXIT_SUCCESS);
+}
+
